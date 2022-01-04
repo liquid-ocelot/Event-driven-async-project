06a7ce13146136857074e9fff8a3f0527238a692
Maxence CHAMBRIN <maxence.chambrin@gmail.com>
2022-01-04 16:06:19 +0100

    add typeorm

---
+++ build/lib/fastify.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fastify_1 = require("fastify");
+exports.fastify = fastify_1.default({ logger: true });
+//# sourceMappingURL=fastify.js.map

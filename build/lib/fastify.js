7a6540395f307ca6b9c389ae3525eddd94b3f066
Maxence CHAMBRIN <maxence.chambrin@gmail.com>
2022-01-04 16:40:08 +0100

    add build/ to gitignore

--- build/lib/fastify.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fastify_1 = require("fastify");
-exports.fastify = fastify_1.default({ logger: true });
-//# sourceMappingURL=fastify.js.map

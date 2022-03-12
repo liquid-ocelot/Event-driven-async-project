143552af11e7a085b386f83d777e19119c74a045
Maxence CHAMBRIN <maxence.chambrin@gmail.com>
2022-03-12 23:11:07 +0100

    add custom error handler

--- src/lib/fastify.ts
+++ src/lib/fastify.ts
@@ -10,3 +10,18 @@ export const fs = fastify({ logger: FASTIFY_LOGGING })
 .register(cookie, {secret:COOKIE_SECRET } as FastifyCookieOptions)
 .register(userRoutes, {prefix:"/user"})
 .register(gameRoutes, {prefix:"/game"})
+.setErrorHandler(function defaultErrorHandler(error, request, reply){
+    if(reply.statusCode < 500){
+        reply.log.info(
+            {res: reply, err:error},
+            error && error.message
+        )
+        void reply.send(error)
+    }else{
+        reply.log.error(
+            {req: request, res: reply, err: error},
+            error && error.message
+        )
+        void reply.send({statusCode:500, error:"Internal Server Error", message:'[TRUNCATED]'})
+    }
+})

9de7422b8efd8845ccad2453755044a5b2546ad5
Maxence CHAMBRIN <maxence.chambrin@gmail.com>
2022-03-13 17:43:10 +0100

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,7 @@ import { fs } from "./lib/fastify";
 import { DATABASE_HOST, DATABASE_LOGGING, DATABASE_NAME, DATABASE_PASS, DATABASE_PORT, DATABASE_SYNC, DATABASE_USER, FASTIFY_PORT } from "./lib/dotenv";
 
 async function run() {
-	console.log({
-		type: "postgres",
-		host: DATABASE_HOST,
-		port: DATABASE_PORT,
-		username: DATABASE_USER,
-		password: DATABASE_PASS,
-		database: DATABASE_NAME,
-		synchronize: DATABASE_SYNC,
-		logging: DATABASE_LOGGING,
-	});
+
 	await createConnection({
 		type: "postgres",
 		host: DATABASE_HOST,
